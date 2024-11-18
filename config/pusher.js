90d076b07103626cbc592a9d984a1af67303eea4
Sandile <83593288+sandile-nkosi@users.noreply.github.com>
2024-11-18 15:39:39 +0200

    realtime votes update

--- config/pusher.js
+++ config/pusher.js
@@ -1,10 +1,12 @@
 const Pusher = require("pusher");
+const dotenv = require("dotenv").config();
+
 
 const pusher = new Pusher({
-  appId: "1897597",
-  key: "6cfc796921d6b404e425",
-  secret: "8393cb43b8117a517920",
-  cluster: "ap2",
+  appId: process.env.APPID,
+  key: process.env.KEY,
+  secret: process.env.SECRET,
+  cluster: process.env.CLUSTER,
   useTLS: true
 });
 
